25081ab982ec1cf6bd02b83e89f8811e7066128d
Austin Main <austinfarr2@Austins-MBP-2.localdomain>
2025-04-29 09:52:34 -0500

    Loosened build rules

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,104 +1,105 @@
-"use client";
+'use client';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Menu } from "lucide-react";
-import { Card } from "@/components/ui/card";
-import { ThemeToggle } from "@/components/layout/ThemeToggle";
-import { Button } from "@/components/ui/button";
+} from '@/components/ui/dropdown-menu';
+import { Menu } from 'lucide-react';
+import { Card } from '@/components/ui/card';
+import { ThemeToggle } from '@/components/layout/ThemeToggle';
+import { Button } from '@/components/ui/button';
 // import ShadcnKit from "@/components/icons/shadcn-kit";
 // import { randomUUID } from "randomUUID";
-import Link from "next/link";
+import Link from 'next/link';
 // import { randomUUID } from "crypto";
-import { usePathname } from "next/navigation";
-import SearchBar from "../SearchBar";
+import { usePathname } from 'next/navigation';
+import SearchBar from '../SearchBar';
 
 const Navbar = ({}) => {
   const pathname = usePathname();
-  console.log("pathname", pathname);
 
   return (
-    <Card className="bg-card py-3 px-8 border-0 flex items-center justify-between gap-6 rounded-2xl mt-5">
-      {/* Logo here */}
-      <ul className="hidden md:flex items-center gap-10 text-card-foreground">
-        <p className="text-2xl font-bold text-primary">ReadThat</p>
-        <li
-          className={`text-primary font-medium ${
-            pathname === "/" ? "text-primary" : "text-black"
-          }`}
-        >
-          <Link href="/">Home</Link>
-        </li>
-        <li
-          className={`text-primary font-medium ${
-            pathname === "/explore" ? "text-primary" : "text-black"
-          }`}
-        >
-          <a href="/explore">Explore</a>
-        </li>
-      </ul>
+    <div className="px-4 my-4">
+      <Card className="bg-card py-3 px-8 border-0 flex items-center justify-between gap-6 rounded-2xl mt-5">
+        {/* Logo here */}
+        <ul className="hidden md:flex items-center gap-10 text-card-foreground">
+          <p className="text-2xl font-bold text-primary">ReadThat</p>
+          <li
+            className={`text-primary font-medium ${
+              pathname === '/' ? 'text-primary' : 'text-black'
+            }`}
+          >
+            <Link href="/">Home</Link>
+          </li>
+          <li
+            className={`text-primary font-medium ${
+              pathname === '/explore' ? 'text-primary' : 'text-black'
+            }`}
+          >
+            <a href="/explore">Explore</a>
+          </li>
+        </ul>
 
-      <SearchBar />
+        <SearchBar />
 
-      <div className="flex items-center">
-        <Button variant="secondary" className="hidden md:block px-2">
-          Login
-        </Button>
-        <Button className="hidden md:block ml-2 mr-2">Get Started</Button>
+        <div className="flex items-center">
+          <Button variant="secondary" className="hidden md:block px-2">
+            Login
+          </Button>
+          <Button className="hidden md:block ml-2 mr-2">Get Started</Button>
 
-        <div className="flex md:hidden mr-2 items-center gap-2">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <span className="py-2 px-2 bg-gray-100 rounded-md">Pages</span>
-            </DropdownMenuTrigger>
+          <div className="flex md:hidden mr-2 items-center gap-2">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <span className="py-2 px-2 bg-gray-100 rounded-md">Pages</span>
+              </DropdownMenuTrigger>
 
-            {/* <DropdownMenuContent align="start">
+              {/* <DropdownMenuContent align="start">
               {landings.map((page) => (
                 <DropdownMenuItem key={page.id}>
                   <Link href={page.route}>{page.title}</Link>
                 </DropdownMenuItem>
               ))}
             </DropdownMenuContent> */}
-          </DropdownMenu>
+            </DropdownMenu>
 
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" size="icon">
-                <Menu className="h-5 w-5 rotate-0 scale-100" />
-              </Button>
-            </DropdownMenuTrigger>
-
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem>
-                <a href="#home">Home</a>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <a href="#features">Features</a>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <a href="#pricing">Pricing</a>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <a href="#faqs">FAQs</a>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Button variant="secondary" className="w-full text-sm">
-                  Login
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="icon">
+                  <Menu className="h-5 w-5 rotate-0 scale-100" />
                 </Button>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Button className="w-full text-sm">Get Started</Button>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
+              </DropdownMenuTrigger>
 
-        <ThemeToggle />
-      </div>
-    </Card>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>
+                  <a href="#home">Home</a>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <a href="#features">Features</a>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <a href="#pricing">Pricing</a>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <a href="#faqs">FAQs</a>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Button variant="secondary" className="w-full text-sm">
+                    Login
+                  </Button>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Button className="w-full text-sm">Get Started</Button>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+
+          <ThemeToggle />
+        </div>
+      </Card>
+    </div>
   );
 };
 
