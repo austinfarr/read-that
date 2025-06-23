65f86d8e0ce5e4e758a6d782e6076481eeb6a21d
Austin Farr <austin@farrnet.com>
2025-06-22 23:07:31 -0500

    fix: last profile pic fix of the night

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "@/components/layout/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/layout/Navbar";
+import NavbarWrapper from "@/components/layout/NavbarWrapper";
 import ApolloClientProvider from "@/components/providers/ApolloClientProvider";
 
 const geistSans = Geist({
@@ -37,7 +37,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <ApolloClientProvider>
-            <Navbar />
+            <NavbarWrapper />
             {children}
           </ApolloClientProvider>
         </ThemeProvider>
